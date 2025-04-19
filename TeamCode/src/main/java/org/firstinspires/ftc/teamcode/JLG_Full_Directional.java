270931fbba737b4f4c194dfd5e3ce5f94f929d99
FTC-Thunderwolves <gilb0210@gmail.com>
2025-04-19 00:09:42 -0500

    Making Buttons Do Things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JLG_Full_Directional.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JLG_Full_Directional.java
@@ -40,8 +40,8 @@ public class JLG_Full_Directional extends LinearOpMode {
             touchSensor  = hardwareMap.get(TouchSensor.class, "touchSensor");
 
             // Get gamepad input
-            double forward = gamepad1.left_stick_y; // Forward/Backward
-            double turn = gamepad1.right_stick_x;   // Left/Right turning
+            double forward = -gamepad1.left_stick_y; // Forward/Backward
+            double turn = -gamepad1.right_stick_x;   // Left/Right turning
 
             //Speed Modifiers
             double stopPower = 0;
@@ -61,9 +61,11 @@ public class JLG_Full_Directional extends LinearOpMode {
 
             boolean isPressed = touchSensor.isPressed();
             if(isPressed) {
-                leftDrive.setPower(-0.5);
-                rightDrive.setPower(-0.5);
+                leftDrive.setPower(0.5);
+                rightDrive.setPower(0.5);
                 sleep(2000);
+                leftDrive.setPower(0);
+                rightDrive.setPower(0);
               }
 
             /* Making Buttons do Stuff */
@@ -71,8 +73,8 @@ public class JLG_Full_Directional extends LinearOpMode {
                 leftDrive.setPower(stopPower);
                 rightDrive.setPower(stopPower);
             } else if (gamepad1.right_bumper) { //Press RIGHT BUMPER to SPEED UP
-                leftDrive.setPower(leftPower * 1.5);
-                rightDrive.setPower(rightPower * 1.5);
+                leftDrive.setPower(leftPower);
+                rightDrive.setPower(rightPower);
             } else if (gamepad1.left_bumper) { //Press LEFT BUMPER to SLOW DOWN
                 leftDrive.setPower(leftPower * 0.5);
                 rightDrive.setPower(rightPower * 0.5);
@@ -99,50 +101,57 @@ public class JLG_Full_Directional extends LinearOpMode {
                         sleep(2000);
                         rightDrive.setPower(0);
 
-                        break; }
+                        break;
+                    }
 
                 }
 
 
-
+            }
 
                 //Display Encoder Values on Drive Station
-                    telemetry.addData("Left Motor Encoder", leftEncoder);
-                    telemetry.addData("Right Motor Encoder", rightEncoder);
+                telemetry.addData("Left Motor Encoder", leftEncoder);
+                telemetry.addData("Right Motor Encoder", rightEncoder);
 
                 // Send telemetry data
                 telemetry.addData("Left Power", leftPower);
                 telemetry.addData("Right Power", rightPower);
 
 
-                    if (gamepad1.a) {
-                        telemetry.addData("Button Pressed", "A");
+                if (gamepad1.a) {
+                    telemetry.addData("Button Pressed", "A");
                     }
-                    else if (gamepad1.b) {
-                        telemetry.addData("Button Pressed", "B");
+                else if (gamepad1.b) {
+                    telemetry.addData("Button Pressed", "B");
                     }
-                    else if (gamepad1.x) {
-                        telemetry.addData("Button Pressed", "X");
+                else if (gamepad1.x) {
+                    telemetry.addData("Button Pressed", "X");
                     }
-                    else if (gamepad1.y) {
-                        telemetry.addData("Button Pressed", "Y");
-                    } else if (gamepad1.left_bumper) {
+                else if (gamepad1.y) {
+                    telemetry.addData("Button Pressed", "Y");
+                    }
+                else if (gamepad1.left_bumper) {
                         telemetry.addData("Button Pressed", "Left Bumper");
-                    } else if (gamepad1.right_bumper) {
+                    }
+                else if (gamepad1.right_bumper) {
                         telemetry.addData("Button Pressed", "Right Bumper");
-                    } else if (gamepad1.left_trigger > 0.1) {
+                    }
+                else if (gamepad1.left_trigger > 0.1) {
                         telemetry.addData("Button Pressed", "Left Trigger");
-                        } else if (gamepad1.right_trigger > 0.1) {
+                        }
+                else if (gamepad1.right_trigger > 0.1) {
                         telemetry.addData("Button Pressed", "Right Trigger");
-                    } else {
+                    }
+                else {
                         telemetry.addData("Button Pressed", "None");
                     }
-                    telemetry.update();//move???>??
+
+                telemetry.update();//move???>??
 
 
                 }
             }
         }
-    }
+
 
 
