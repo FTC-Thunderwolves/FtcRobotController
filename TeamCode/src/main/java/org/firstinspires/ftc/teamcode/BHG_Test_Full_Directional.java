5f209baf06847ed4ccd2f466922a6d964c271cf8
FTC-Thunderwolves <gilb0210@gmail.com>
2025-04-19 13:09:02 -0500

    Ben's code that does stuff...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BHG_Test_Full_Directional.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BHG_Test_Full_Directional.java
@@ -3,7 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp(name = "BHG_Test_Full_Directional", group = "TeleOp")
 public class BHG_Test_Full_Directional extends LinearOpMode {
@@ -12,7 +15,7 @@ public class BHG_Test_Full_Directional extends LinearOpMode {
     private DcMotor rightDrive;
 
     TouchSensor touchSensor; //Have to do this somewhere I guess....
-
+    DistanceSensor distanceSensor;
     @Override
     public void runOpMode() {
         // Initialize motors
@@ -25,17 +28,16 @@ public class BHG_Test_Full_Directional extends LinearOpMode {
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-        leftDrive.setPower(0.5);
-        rightDrive.setPower(0.5);
-        sleep(3000);
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
+
         // Wait for driver to press the start button
+        telemetry.addData("Status", "Waiting for Start");
+        telemetry.update();
+
         waitForStart();
 
         while (opModeIsActive()) {
            touchSensor  = hardwareMap.get(TouchSensor.class, "touchSensor"); //You could instead do TouchSensor touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
-
+            distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
             // Get gamepad input
             if (gamepad1.x) {
                 leftDrive.setPower(0);
@@ -46,27 +48,26 @@ public class BHG_Test_Full_Directional extends LinearOpMode {
             double forward = gamepad1.left_stick_y; // Forward/Backward
             double turn = gamepad1.left_stick_x;   // Left/Right turning
             // Calculate motor power
-            boolean leftTrigger = gamepad1.left_bumper;
-            boolean rightTrigger = gamepad1.right_bumper;
+            boolean leftBumper = gamepad1.left_bumper;
+            boolean rightBumper = gamepad1.right_bumper;
             double leftPower = forward + turn;
             double rightPower = forward - turn;
 
             // Apply power to motors
-            if (leftTrigger) {
-                if (rightTrigger) {
+            if (leftBumper) {
+                if (rightBumper) {
                     leftDrive.setPower(leftPower * 1);
                     rightDrive.setPower(rightPower * 1);
                 } else {
                     leftDrive.setPower(leftPower * 0.25);
                     rightDrive.setPower(rightPower * 0.25);
                 }
-            } else if (!leftTrigger) {
+            } else if (!leftBumper) {
                 leftDrive.setPower(leftPower * 0.5);
                 rightDrive.setPower(rightPower * 0.5);
             }
 
-            telemetry.addData("Status", "Waiting for Start");
-            telemetry.update();
+
             // Check if the sensor is pressed
             boolean isPressed = touchSensor.isPressed();
             if (isPressed) {
@@ -76,9 +77,11 @@ public class BHG_Test_Full_Directional extends LinearOpMode {
                 leftDrive.setPower(0);
                 rightDrive.setPower(0);
             }
+            if(distanceSensor)
             // Display on Driver Station
+
+            telemetry.addData("Distance Sensor", distanceSensor.getDistance(DistanceUnit.INCH));
             telemetry.addData("Touch Sensor", isPressed ? "PRESSED" : "NOT PRESSED");
-            telemetry.update();
 
             // Send telemetry data
             telemetry.addData("Left Power", leftPower);
