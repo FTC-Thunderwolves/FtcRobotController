f017f763aebe9ce712d3476d95b654ac31026333
FTC-Thunderwolves <gilb0210@gmail.com>
2025-04-20 08:09:55 -0500

    Ben's code that does stuff...
    4/19/25 - I added a distance sensor that backs away whenever it gets closer than a foot to something
    
    4/20/25 - I finished the Autonomous! Now, the robot will drive forward and if anything gets in the range of about 12 inches, it will back up and turn, and the robot will continue in that loop. WARNING THOUGH, if something gets in the way on the side, the robot might not be able to detect the object.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BHG_Object_Avoidance.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name = "Object Avoidance", group = "Autonomous")
+public class BHG_Object_Avoidance extends LinearOpMode {
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
+    DistanceSensor distanceSensor;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
+        // Set motor direction (adjust based on physical setup)
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double distance = distanceSensor.getDistance(DistanceUnit.INCH);
+
+            if (Double.isNaN(distance)) {
+                telemetry.addData("Distance", "Invalid");
+                telemetry.update();
+                continue; // Skip loop iteration if the distance is invalid
+            }
+
+            telemetry.addData("Distance", distance);
+            telemetry.update();
+
+            if (distance > 12) {
+                // Move forward
+                leftDrive.setPower(-0.35);
+                rightDrive.setPower(-0.35);
+            } else if (distance <= 12) {
+                // Reverse when too close
+                leftDrive.setPower(0.25);
+                rightDrive.setPower(0.25);
+                sleep(1500);
+                leftDrive.setPower(-0.5);
+                rightDrive.setPower(0);
+                sleep(1000);
+            }
+            telemetry.addData("Distance", distance);
+            telemetry.update();
+        }
+    }
+}
