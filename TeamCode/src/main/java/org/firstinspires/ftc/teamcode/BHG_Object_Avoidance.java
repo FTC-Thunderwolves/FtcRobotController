9686311bf4761cc858232890c16783dfe395ca4d
FTC-Thunderwolves <gilb0210@gmail.com>
2025-04-20 08:11:22 -0500

    Ben's code that does stuff...
    4/19/25 - I added a distance sensor that backs away whenever it gets closer than a foot to something
    
    4/20/25 - I finished the Autonomous! Now, the robot will drive forward and if anything gets in the range of about 12 inches, it will back up and turn, and the robot will continue in that loop. WARNING THOUGH, if something gets in the way on the side, the robot might not be able to detect the object.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BHG_Object_Avoidance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BHG_Object_Avoidance.java
@@ -13,7 +13,6 @@ public class BHG_Object_Avoidance extends LinearOpMode {
     private DcMotor rightDrive;
     DistanceSensor distanceSensor;
 
-
     @Override
     public void runOpMode() throws InterruptedException {
         leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
